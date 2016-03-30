8bf1613313cd7d7dc5f1ae86e05561601ee5365b
Alexander Khomenko <a.khomenko@protonmail.com>
2016-03-30 14:09:10 +0300

    + filter for pokemon types; ~ additional styling and spinner position fix

--- script/base.js
+++ script/base.js
@@ -1,30 +1,47 @@
 var domainName = "http://pokeapi.co",
     pokemonUrl = "/api/v1/pokemon/",
-    defaultUrl = "/api/v1/pokemon/?limit=10";
+    defaultUrl = "/api/v1/pokemon/?limit=22";
 
 $(document).ready(function () {
+    var $pokemonsContainer = $("#pokemonsContainer");
+
     getPokemons();
 
     $("#loadMore").click(function (element) {
         togglePreventClick(true);
 
-        getPokemons($(element.target).attr('data-url'));
+        showAll();
+        getPokemons($(element.target).attr("data-url"));
     });
 
-    $("#pokemonsContainer").on("click", ".pokemonContainer img", function (pokemon) {
-        var id = $(pokemon.target).parent().attr('data-id'),
+    $pokemonsContainer.on("click", ".pokemonContainer img", function (pokemon) {
+        var id = $(pokemon.target).parent().attr("data-id"),
             url = pokemonUrl + id;
 
         togglePreventClick(true);
 
         getPokemon(url);
     });
+
+    $pokemonsContainer.on("click", ".tp", function (element) {
+        var type = $(element.target).attr("data-type");
+
+        $pokemonsContainer.find(".pokemonContainer:not(:has([data-type=" + type + "]))").hide();
+    });
+
+    // onclick display all
+    $("#logo").on("click", function () {
+        showAll();
+    });
 });
 
-var togglePreventClick = function (add) {
-    $("body").toggleClass('prevent-click', add);
-}
+var showAll = function () {
+    $("#pokemonsContainer").find(".pokemonContainer").show();
+};
 
+var togglePreventClick = function (add) {
+    $("body").toggleClass("prevent-click", add);
+};
 
 var getPokemons = function (url) {
     url = url || defaultUrl;
@@ -43,18 +60,15 @@ var getPokemons = function (url) {
                 item.types.forEach(function (type) {
                     var $pokemonType = getTemplate("pokemonTypeTemplate");
                     $pokemonType.attr("data-type", type.name);
-                    $pokemonType.attr("class", "tp tp-" + type.name);
+                    $pokemonType.addClass("tp-" + type.name);
                     $pokemonType.html(type.name);
                     $pokemonContainer.find(".type").append($pokemonType);
-                    /*if($pokemonContainer.find(".type p").attr("data-type") === type.name){
-                     $pokemonContainer.find(".type p").addClass("tp tp-" + type.name);
-                     }*/
                 });
 
                 $("#pokemonsContainer").append($pokemonContainer);
             });
 
-            $('#loadMore').attr('data-url', data.meta.next);
+            $("#loadMore").attr("data-url", data.meta.next);
 
             togglePreventClick(false);
         });
@@ -71,14 +85,6 @@ var getPokemon = function (pokemon) {
 
             $pokemonPopup.find(".name").html(data.name);
             $pokemonPopup.find("img").attr("src", "http://pokeapi.co/media/img/" + data.pkdx_id + ".png");
-            /*Object.keys(data).forEach(function (key) {
-             var stat = data[key],
-             $el = $pokemonPopup.find(".stats ." + key);
-
-             if($el.length) {
-             $el.html(stat);
-             }
-             });*/
 
             data.types.forEach(function (type) {
                 $pokemonPopup.find(".stats .types").append(type.name + " ");
@@ -94,7 +100,7 @@ var getPokemon = function (pokemon) {
 
             $("#myModal .modal-dialog").html($pokemonPopup);
 
-            $('#myModal').modal("toggle");
+            $("#myModal").modal("toggle");
 
             togglePreventClick(false);
         });
